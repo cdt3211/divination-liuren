28ede81a5f5a016ef6d7b08c65a77457201e4d61
Abner <1228428816@qq.com>
2024-11-17 10:57:58 +0800

    🐞 fix: 修复bug

--- src/utils/op.js
+++ src/utils/op.js
@@ -1,7 +1,7 @@
 import { OpenPanel } from '@openpanel/web';
 
 export const op = new OpenPanel({
-  clientId: 'a6c07e88-9aac-4205-8ec8-259b9852f31b',
+  clientId: process.env.OPENPANEL_CLIENTID,
   trackScreenViews: true,
   trackOutgoingLinks: true,
   trackAttributes: true,
